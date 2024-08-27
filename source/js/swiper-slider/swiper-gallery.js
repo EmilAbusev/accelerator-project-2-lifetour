0d4195df4084e63fb1e3c98539fdbcca6a50abba
Emil Abusev <emilabusev.yandex.ru>
2024-08-27 16:53:07 +0300

    Загружает изображения для блока(gallery). Добавляет свайпер для блока(gallery). Завершает стилизацию блока(gallery).

--- source/js/swiper-slider/swiper-gallery.js
+++ source/js/swiper-slider/swiper-gallery.js
@@ -7,5 +7,44 @@ const swiperSlider = document.querySelector('.slider-gallery');
 const swiperButtonPrev = document.querySelector('.gallery__navigation-prev');
 const swiperButtonNext = document.querySelector('.gallery__navigation-next');
 
+let mySwiper;
+
+const initGallerySwiper = () => {
+  if (window.innerWidth <= 1439 && swiperSlider.dataset.desktop === 'false') {
+    mySwiper = new Swiper(swiperSlider, {
+      modules: [Navigation],
+      navigation: {
+        nextEl: swiperButtonNext,
+        prevEl: swiperButtonPrev,
+      },
+      breakpoints: {
+        320: {
+          slidesPerView: 2,
+          spaceBetween: 5,
+          initialSlide: 0,
+        },
+        768: {
+          slidesPerView: 3,
+          spaceBetween: 5,
+          initialSlide: 0,
+        },
+      },
+    });
+
+    swiperSlider.dataset.desktop = 'true';
+  }
+
+  if (window.innerWidth > 1439) {
+    swiperSlider.dataset.desktop = 'false';
+    if (swiperSlider.classList.contains('.swiper-initialized')) {
+      mySwiper.destroy(true, true);
+    }
+  }
+};
+
+window.addEventListener('resize', () => {
+  initGallerySwiper();
+});
+
 
 export { initGallerySwiper };
